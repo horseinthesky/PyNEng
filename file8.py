a64f8e0206010bf52dd83bcea92b1fe81044f23c
horseinthesky <pwnedbyspawn@gmail.com>
2017-06-22 23:35:57 +0300

    22.06.2017

--- file8.py
+++ file8.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
-access_template = ['switchport mode access', 'switchport access vlan', 'spanning-tree portfast', 'spanning-tree bpduguard enable']
+access_template = ['switchport mode access', 'switchport access vlan',
+                   'spanning-tree portfast', 'spanning-tree bpduguard enable']
 
-fast_int = {'access':{'0/12':'10','0/14':'11','0/16':'17','0/17':'150'}}
+fast_int = {'access': {'0/12': '10', '0/14': '11', '0/16': '17', '0/17': '150'}}
 
 for int in fast_int['access']:
     print 'interface FastEthernet' + int
@@ -10,4 +11,3 @@ for int in fast_int['access']:
             print ' %s %s' % (command, fast_int['access'][int])
         else:
             print ' %s' % command
-
