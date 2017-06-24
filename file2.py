33819458b84ee782ae09d7c5d17e137e789b2709
horseinthesky <pwnedbyspawn@gmail.com>
2017-06-24 10:46:52 -0700

    tast3.1

---
+++ file2.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+print ('Привет')
+print ('Пойду почитаю xgu.ru :)')
+print (input ('Твой любимый протокол маршрутизации? ' ))
--- task3.1.py
+++ task3.1.py
@@ -1,19 +1,20 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-'''
+print('''
 Задание 3.1
 Обработать строку ospf_route и вывести информацию в виде:
-Protocol:				OSPF
-Prefix:					10.0.24.0/24
-AD/Metric:				110/41
-Next-Hop:				10.0.13.3
+Protocol:			OSPF
+Prefix:				10.0.24.0/24
+AD/Metric:			110/41
+Next-Hop:			10.0.13.3
 Last update:			3d18h
 Outbound Interface:		FastEthernet0/0
-'''
+''')
 
-route_raw = "O        10.0.24.0/24 [110/41] via 10.0.13.3, 3d18h, FastEthernet0/0"
+ospf_route = "O        10.0.24.0/24 [110/41] via 10.0.13.3, 3d18h, FastEthernet0/0"
+print(('ospf_route = '), ospf_route,'\n')
 
-commands = route_raw.split()
+commands = ospf_route.split()
 
 print('Protocol: \t\t' + commands[0])
 print('Prefix:\t\t\t' + commands[1])
