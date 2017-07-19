f59e69b90f6e8f6080af73a8a55bac65951aa705
horseinthesky <pwnedbyspawn@gmail.com>
2017-07-19 07:58:40 -0700

    9.2 done

---
+++ 7.Functions/task74a.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+#-*- coding: utf-8 -*-
+
+"""
+Задание 7.4a
+Задача такая же, как и задании 7.4. Проверить работу функции надо на примере файла config_r1.txt
+
+Обратите внимание на конфигурационный файл. В нем есть разделы с большей вложенностью, например, разделы:
+	• interface Ethernet0/3.100
+	• router bgp 100
+
+Надо чтобы функция config_to_dict обрабатывала следующий уровень вложенности. При этом, не привязываясь к конкретным разделам. Она должна быть универсальной, и сработать, если это будут другие разделы.
+
+Теперь:
+	• если уровня 2, то команды верхнего уровня будут ключами словаря, а команды подуровней - списками;
+	• если уровня 3, то самый вложенный должен быть списком, а остальные - словарями.
+
+На примере interface Ethernet0/3.100
+{'interface Ethernet0/3.100':{
+                    'encapsulation dot1Q 100':[],
+                    'xconnect 10.2.2.2 12100 encapsulation mpls':
+                        ['backup peer 10.4.4.4 14100',
+                         'backup delay 1 1']}}
+"""
+
+ignore = ['duplex', 'alias', 'Current configuration']
+
+def check_ignore(command, ignore):
+    """
+    Функция проверяет содержится ли в команде слово из списка ignore.
+    
+    command - строка. Команда, которую надо проверить
+    ignore - список. Список слов
+    
+    Возвращает True, если в команде содержится слово из списка ignore, False - если нет
+    """
+    ignore_command = False
+
+    for word in ignore:
+        if word in command:
+            ignore_command = True
+
+    return ignore_command
+
+def config_to_dict(config):
+    """
+    config - имя конфигурационного файла
+    """
+    config_dict = {}
+
+    with open(config) as f:
+        for line in f:
+            line = line.rstrip()
+            if line and not (line.startswith('!') or check_ignore(line, ignore)):
+                if line[0].isalnum():
+                    section = line
+                    config_dict[section] = []
+                elif line.startswith(' ') and not line.startswith('  '):
+                    options = line
+                    config_dict[section].append(line)
+                elif line.startswith('  '):
+                    config_dict[section] = { i: [] for i in config_dict[section] }
+                    config_dict[section][options].append(line)
+
+    return config_dict
+
+print(config_to_dict('config_r1.txt'))
---
+++ 8.Modules/task8.2a.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+#-*- coding: utf-8 -*-
+
+"""
+С помощью функции parse_cdp_neighbors из задания 8.2 и 
+функции draw_topology из файла draw_network_graph.py, 
+сгенерировать топологию, которая соответствует выводу 
+команды sh cdp neighbor в файле sw1_sh_cdp_neighbors.txt
+
+Не копировать код функций parse_cdp_neighbors и draw_topology.
+
+В итоге, должен быть сгенерировано изображение топологии. 
+Результат должен выглядеть так же, как схема в файле task_8_2a_topology.svg
+"""
+
+"""Решение"""
+from task82 import parse_cdp_neighbors
+from draw_network_graph import draw_topology
+
+print(draw_topology(parse_cdp_neighbors('sw1_sh_cdp_neighbors.txt')))
