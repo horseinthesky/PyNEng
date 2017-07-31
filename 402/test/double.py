71c1c216ad1244cbaa4c2a9405877f36ad250d90
horseinthesky <pwnedbyspawn@gmail.com>
2017-07-31 00:06:49 -0700

    work on 402 vpn contionue

--- 402/test/double.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-#-*- coding: utf-8 -*-
-
-with open('result402.txt') as f:
-    for line in f:
-        with open('nonsber.txt') as m:
-            for string in m:
-                if not string == line:
-                    print(line)
--- 402/test/double2.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-#-*- coding: utf-8 -*-
-
-with open('test.txt') as f:
-    f = f.read().split('\n')
-    set_test = {i for i in f}
-    for i in set_test:
-        print(i)
